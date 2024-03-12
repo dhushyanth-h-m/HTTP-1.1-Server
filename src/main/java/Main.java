12c0bedce7ff1c6dba1cd1d106562a19bed28d90
Dhushyanth H M <dxh8160@mavs.uta.edu>
2024-03-12 03:50:21 -0500

    Parse headers and send a corresponding response body

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
@@ -26,6 +27,18 @@ public class Main {
            var s = reader.readLine();
            var parts = s.split("\\s+");
 
+           String line;
+           StringBuilder headerBuilder = new StringBuilder();
+
+           // Read and append each line of the HTTP request headers
+           while ((line = reader.readLine()) != null && !line.isEmpty()) {
+               headerBuilder.append(line).append("\r\n");
+           }
+
+           String headers = headerBuilder.toString();
+           String[] headerLines = headers.split("\r\n");
+           System.out.println(Arrays.toString(headerLines));
+
            if(parts.length >= 2 && "/".equals(parts[1])){
                 sendOk(clientSocket);
            }
@@ -33,6 +46,11 @@ public class Main {
                String message = parts[1].substring("/echo".length());
                message = message.replaceAll("^[/\\s]+", "");
                sendResponse(clientSocket, message);
+           }
+           else if(parts.length >= 2 && parts[1].matches("^/user-agent.*")){
+               String message = headerLines[1].substring("User-Agent: ".length())
+                       .replaceAll("^[/\\s]+", "");
+               sendHeaderResponse(clientSocket, message);
            }
            else{
                send404(clientSocket);
@@ -67,6 +85,15 @@ public class Main {
                 "\r\n" + // Blank line to end headers
                 message; // Response body
         response.write(responseHeaders.getBytes());
+    }
 
+    public static void sendHeaderResponse(Socket clientSocket, String message) throws IOException{
+        OutputStream response = clientSocket.getOutputStream();
+        String responseHeaders = "HTTP/1.1 200 OK\r\n" + // Status line
+                "Content-Type: text/plain\r\n" +
+                "Content-Length: " + message.length() + "\r\n" + // Header
+                "\r\n" + // Blank line to end headers
+                message; // Response body
+        response.write(responseHeaders.getBytes());
     }
 }
