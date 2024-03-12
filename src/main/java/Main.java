abb3fa698e95e61acb54bf82f947d17511de3766
Dhushyanth H M <dxh8160@mavs.uta.edu>
2024-03-12 03:22:40 -0500

    Send response with content

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
@@ -28,6 +29,11 @@ public class Main {
            if(parts.length >= 2 && "/".equals(parts[1])){
                 sendOk(clientSocket);
            }
+           else if(parts.length >=2 && parts[1].matches("^/echo.*")){
+               String message = parts[1].substring("/echo".length());
+               message = message.replaceAll("^[/\\s]+", "");
+               sendResponse(clientSocket, message);
+           }
            else{
                send404(clientSocket);
            }
@@ -48,8 +54,20 @@ public class Main {
     }
 
     public static void send404(Socket clientSocket) throws IOException{
+       OutputStream response = clientSocket.getOutputStream();
+       String responseHeaders = "HTTP/1.1 404 Not Found\r\n\r\n";
+       response.write(responseHeaders.getBytes());
+    }
+
+    public static void sendResponse(Socket clientSocket, String message) throws IOException{
         OutputStream response = clientSocket.getOutputStream();
-        String responseHeaders = "HTTP/1.1 404 Not Found\r\n\r\n";
+        String responseBody = message;  // Example response body
+        String responseHeaders = "HTTP/1.1 200 OK\r\n" + // Status line
+                "Content-Type: text/plain\r\n" +
+                "Content-Length: " + responseBody.length() + "\r\n" + // Header
+                "\r\n" + // Blank line to end headers
+                responseBody; // Response body
         response.write(responseHeaders.getBytes());
+
     }
 }
