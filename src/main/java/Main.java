0102e697f8f9572774a6b66cd050f06a92fd2a1e
Dhushyanth H M <dxh8160@mavs.uta.edu>
2024-03-12 02:31:04 -0500

    Responds with 200 OK

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,13 +10,30 @@ public class Main {
      ServerSocket serverSocket = null;
      Socket clientSocket = null;
 
+     OutputStream response = null;
+
      try {
        serverSocket = new ServerSocket(4221);
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
+
+       response = clientSocket.getOutputStream();
+       String responseHeaders = "HTTP/1.1 200 OK\r\n\r\n";
+       response.write(responseHeaders.getBytes());
+
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
+     }
+     finally {
+         if(response != null){
+             try{
+                 response.close();
+             }
+             catch(Exception e){
+                 System.out.println("IO Exception"+e);
+             }
+         }
      }
   }
 }
