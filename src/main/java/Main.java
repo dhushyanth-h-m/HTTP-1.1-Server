076cf5434ed99b1ca5630304838d9745bccde339
Dhushyanth H M <dxh8160@mavs.uta.edu>
2024-03-12 03:09:46 -0500

    Responsds with 404 for different paths

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -18,22 +20,36 @@ public class Main {
        clientSocket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
 
-       response = clientSocket.getOutputStream();
-       String responseHeaders = "HTTP/1.1 200 OK\r\n\r\n";
-       response.write(responseHeaders.getBytes());
+       InputStreamReader inputStream = new InputStreamReader(clientSocket.getInputStream());
+       try(BufferedReader reader = new BufferedReader(inputStream)){
+           var s = reader.readLine();
+           var parts = s.split("\\s+");
+
+           if(parts.length >= 2 && "/".equals(parts[1])){
+                sendOk(clientSocket);
+           }
+           else{
+               send404(clientSocket);
+           }
+       }
+       catch (Exception e){
+           System.out.println("IO Exception"+ e);
+       }
 
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
-     finally {
-         if(response != null){
-             try{
-                 response.close();
-             }
-             catch(Exception e){
-                 System.out.println("IO Exception"+e);
-             }
-         }
-     }
   }
+
+    public static void sendOk(Socket clientSocket) throws IOException{
+       OutputStream response = clientSocket.getOutputStream();
+       String responseHeaders = "HTTP/1.1 200 OK\r\n\r\n";
+       response.write(responseHeaders.getBytes());
+    }
+
+    public static void send404(Socket clientSocket) throws IOException{
+        OutputStream response = clientSocket.getOutputStream();
+        String responseHeaders = "HTTP/1.1 404 Not Found\r\n\r\n";
+        response.write(responseHeaders.getBytes());
+    }
 }
